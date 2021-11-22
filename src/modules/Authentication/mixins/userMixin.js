d92aef773ba6f969750b7f66adb0efe7585dad21
matheushsilverio <matheushsilverio@gmail.com>
2021-11-21 22:26:04 -0300

    🐛 arrumando o userMixin

--- src/modules/Authentication/mixins/userMixin.js
+++ src/modules/Authentication/mixins/userMixin.js
@@ -7,18 +7,14 @@ export default {
     ...mapActions("User", ["setUserStore"]),
     authenticateUser(email, password) {
       authenticateService(email, password)
-        .then(response => {
-          console.log(response);
-          const { token, refreshToken, user } = response.data;
-          console.log({ token, refreshToken, user });
+        .then(({ token, refreshToken, user }) => {
           this.setUserStore(user);
           tokenService.save(token);
           tokenService.saveRefresh(refreshToken);
 
           this.$router.push({ name: "Dashboard" });
         })
-        .catch(err => {
-          console.log(err);
+        .catch(() => {
           alert("Authentication Failed");
         });
     }
